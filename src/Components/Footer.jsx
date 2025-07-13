5ffa5ce903fc45ccee1901aa822eed87d7fae01c
Daksh Shahani <dakshitshahani@gmail.com>
2025-07-13 16:33:37 +0300

    created Header, Footer Note Components

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return <footer>
+        <p>Copyright © {new Date().getFullYear()} Keeper</p>
+    </footer>
+}
+
+
+export default Footer;
---
+++ src/Components/Note.jsx
@@ -0,0 +1,9 @@
+
+function Note() {
+    return <div className='note'>
+        <h1>This is the title</h1>
+        <p>This is the content</p>
+    </div>
+}
+
+export default Note;
